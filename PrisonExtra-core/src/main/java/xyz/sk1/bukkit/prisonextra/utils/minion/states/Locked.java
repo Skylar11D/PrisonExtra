965356bdcab5326e785aaa56e7c53f7a0a148ce3
skylar11d <skylar11d@aol.com>
2024-09-15 02:43:37 +0300

    Eighth commitment

---
+++ PrisonExtra-core/src/main/java/xyz/sk1/bukkit/prisonextra/utils/minion/states/Locked.java
@@ -0,0 +1,14 @@
+package xyz.sk1.bukkit.prisonextra.utils.minion.states;
+
+import xyz.sk1.bukkit.prisonextra.miner.State;
+
+public class Locked implements State {
+
+    @Override
+    public void perform() {
+
+
+
+    }
+
+}
