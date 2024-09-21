b4fdfd48c31bfb8e9742331c143f5d3a2e1a2681
skylar11d <skylar11d@aol.com>
2024-09-21 14:04:23 +0300

    [Global] added listeners and events handlers

--- PrisonExtra-core/src/main/java/xyz/sk1/bukkit/prisonextra/events/listeners/position/PlayerMoveListener.java
+++ PrisonExtra-core/src/main/java/xyz/sk1/bukkit/prisonextra/events/listeners/position/PlayerMoveListener.java
@@ -1,6 +1,7 @@
 package xyz.sk1.bukkit.prisonextra.events.listeners.position;
 
 import org.bukkit.event.player.PlayerMoveEvent;
+import xyz.sk1.bukkit.prisonextra.events.handler.position.StareHandler;
 import xyz.sk1.bukkit.prisonextra.listeners.BaseListener;
 import xyz.sk1.bukkit.prisonextra.listeners.PrisonEventHandler;
 
@@ -9,7 +10,7 @@ public class PlayerMoveListener extends BaseListener {
     private final PrisonEventHandler<PlayerMoveEvent> stareHandler;
 
     public PlayerMoveListener(){
-        this.stareHandler = new
+        this.stareHandler = new StareHandler();
     }
 
 }
