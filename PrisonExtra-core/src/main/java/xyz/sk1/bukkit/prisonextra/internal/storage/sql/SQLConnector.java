24661aaef5271c2233b9180e51a6f5190fedfc67
skylar11d <skylar11d@aol.com>
2024-09-27 03:30:32 +0300

    [Global] Tweaks to the SQL connector

--- PrisonExtra-core/src/main/java/xyz/sk1/bukkit/prisonextra/internal/storage/sql/SQLConnector.java
+++ PrisonExtra-core/src/main/java/xyz/sk1/bukkit/prisonextra/internal/storage/sql/SQLConnector.java
@@ -3,7 +3,7 @@ package xyz.sk1.bukkit.prisonextra.internal.storage.sql;
 import com.google.gson.JsonObject;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 import xyz.sk1.bukkit.prisonextra.internal.configuration.ConfigurationHandler;
 import xyz.sk1.bukkit.prisonextra.internal.storage.DatabaseConnector;
 import xyz.sk1.bukkit.prisonextra.internal.storage.Repository;
@@ -19,22 +19,37 @@ public class SQLConnector implements DatabaseConnector {
     private ConfigurationHandler configurationHandler;
     private HikariDataSource dataSource;
 
-    private Repository regionRepository;
-    private Repository usersRepository;
-    private Repository minionsRepository;
-
     public SQLConnector(ConfigurationHandler jsonConfigHandler){
         this.configurationHandler = jsonConfigHandler;
-
-        this.regionRepository = new RegionsRepository(this.getConnection(), "regions");
-        this.usersRepository = new UsersRepository(this.getConnection(), "users");
-        this.minionsRepository = new MinionsRepository(this.getConnection(), "minions");
     }
 
     @Override
     public void connect() {
         Utils.LOG.info("\nConnecting to the database..\n");
 
+        Credentials result = this.getResult();
+
+
+        this.init(result.hostname, result.port, result.database, result.autoconnect, result.username, result.passphrase);
+
+        String regions = result.tables.get("regions").getAsString();
+        String minions = result.tables.get("minions").getAsString();
+        String prisoners = result.tables.get("prisoners").getAsString();
+
+        Repository<?> regionRepository = new RegionsRepository(this.getConnection(), regions);
+        Repository<?> usersRepository = new UsersRepository(this.getConnection(), minions);
+        Repository<?> minionsRepository = new MinionsRepository(this.getConnection(), prisoners);
+
+        regionRepository.validate();
+
+        usersRepository.validate();
+
+        minionsRepository.validate();
+
+    }
+
+    @NotNull
+    private Credentials getResult() {
         JsonObject service = (JsonObject) configurationHandler.get("service");
 
         boolean autoconnect = service.get("autoconnection").getAsBoolean();
@@ -47,34 +62,30 @@ public class SQLConnector implements DatabaseConnector {
 
         String database = databaseObj.get("name").getAsString();
 
-        JsonObject table = databaseObj.get("table").getAsJsonObject();
+        JsonObject tables = databaseObj.get("table").getAsJsonObject();
 
-        String regions = table.get("regions").getAsString();
-        String minions = table.get("minions").getAsString();
-        String prisoners = table.get("prisoners").getAsString();
+        return new Credentials(autoconnect, hostname, port, username, passphrase, database, tables);
+    }
 
+    private static class Credentials {
+        public final boolean autoconnect;
+        public final String hostname;
+        public final int port;
+        public final String username;
+        public final String passphrase;
+        public final String database;
+        public final JsonObject tables;
 
-        HikariConfig config = new HikariConfig();
-
-        config.setJdbcUrl("jdbc:mysql://"+hostname+":"+port+"/"+database+"?autoReconnect="+autoconnect);
-        config.setUsername(username);
-        config.setPassword(passphrase);
-
-        config.setLeakDetectionThreshold(5000);
-        config.setMaximumPoolSize(10);
-        config.setConnectionTimeout(30*1000); // 30 seconds
-        config.setIdleTimeout(1000*60*10); // 10 minutes
-        config.setMaxLifetime(1000*60*30); // 30 minutes
-
-        this.dataSource = new HikariDataSource(config);
-
-
-        regionRepository.validate();
-
-        usersRepository.validate();
-
-        minionsRepository.validate();
-
+        public Credentials(boolean autoconnect, String hostname, int port, String username,
+                           String passphrase, String database, JsonObject config) {
+            this.autoconnect = autoconnect;
+            this.hostname = hostname;
+            this.port = port;
+            this.username = username;
+            this.passphrase = passphrase;
+            this.database = database;
+            this.tables = config;
+        }
     }
 
     @Override
@@ -90,6 +101,22 @@ public class SQLConnector implements DatabaseConnector {
     public void close() {
         if(dataSource != null)
             dataSource.close();
+    }
+
+    private void init(String hostname, int port, String database, boolean autoconnect, String username, String passphrase) {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl("jdbc:mysql://"+ hostname +":"+ port +"/"+ database +"?autoReconnect="+ autoconnect);
+        config.setUsername(username);
+        config.setPassword(passphrase);
+
+        config.setLeakDetectionThreshold(5000);
+        config.setMaximumPoolSize(10);
+        config.setConnectionTimeout(30*1000); // 30 seconds
+        config.setIdleTimeout(1000*60*10); // 10 minutes
+        config.setMaxLifetime(1000*60*30); // 30 minutes
+
+        this.dataSource = new HikariDataSource(config);
     }
 
 }
